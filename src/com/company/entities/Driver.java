97f1ff6edfabafe81e7f8735b64bb1bd034dc1e9
Busu <busurmanbek000@gmail.com>
2022-10-08 03:34:42 +0600

    Part 3 is completed

--- src/com/company/entities/Driver.java
+++ src/com/company/entities/Driver.java
@@ -5,11 +5,27 @@ package com.company.entities;
 public class Driver {
     private final int idDiver;
     private final String name;
-    private final String truckName;
+    private String truckName;
 
     public Driver(int idDiver, String name, String truckName) {
         this.idDiver = idDiver;
         this.name = name;
+        this.truckName = truckName;
+    }
+
+    public int getIdDiver() {
+        return idDiver;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTruckName() {
+        return truckName;
+    }
+
+    public void setTruckName(String truckName) {
         this.truckName = truckName;
     }
 
---
+++ src/com/company/entities/State.java
@@ -0,0 +1,6 @@
+package com.company.entities;
+
+public enum State {
+    BASE, ROUTE, REPAIR
+
+}
